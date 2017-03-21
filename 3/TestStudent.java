0b1199f034d9f144d68cfcca13b3fa2a87e55260
PanupongDave <panpongchmasomboon@gmail.com>
2017-03-21 08:13:05 +0700

    add 3

--- 3/TestStudent.java
+++ 3/TestStudent.java
@@ -26,5 +26,20 @@ public class TestStudent{
       i++;
       System.out.println(i+") "+st.toString());
     }
+    scan.nextLine();
+    System.out.print("Enter Code of search: ");
+    String scd = scan.nextLine();
+    int num = searchCode(sts,scd);
+    System.out.println("Code of" + scd+" : " + num);
+  }
+  public static int searchCode(ArrayList<Student> sts, String scd){
+    int total = 0;
+    for(Student st:sts){
+      String Code = st.getCode();
+      String subCode = Code.substring(7,Code.length());
+      if(scd.equals(subCode))
+        total++;
+    }
+    return total;
   }
 }
